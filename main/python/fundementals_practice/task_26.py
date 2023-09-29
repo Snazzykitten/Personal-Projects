e361fd105b0137b7dc49506d6f13a443b61d1959
SnazzyKitten <snazzykitten@proton.me>
2023-09-29 14:22:47 +0000

    Updated

---
+++ main/python/fundementals_practice/task_26.py
@@ -0,0 +1,11 @@
+""" Mean average """
+# Creating a list
+numbers = []
+
+# Preforming logic
+for amount in range(0, 10):
+    numbers.append(int(input("Enter a number: ")))
+
+# Printing average
+print("Average is ", sum(numbers) / 10)
+
